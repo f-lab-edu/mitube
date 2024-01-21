925f2aef18e6472b2b921a200d8ad5fc5cb7eef6
misim3 <misim@student.42seoul.kr>
2024-01-21 23:09:35 +0900

    feat: swagger api 설정

--- mitube-app/src/main/java/com/misim/controller/UserController.java
+++ mitube-app/src/main/java/com/misim/controller/UserController.java
@@ -1,20 +1,20 @@
 package com.misim.controller;
 
-import com.misim.dto.UserDto;
+import com.misim.controller.model.UserDto;
+import com.misim.exception.CommonResponse;
 import com.misim.service.UserService;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.misim.util.HttpStatusResponseEntity.*;
-
+@Tag(name = "User API", description = "User API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users/")
@@ -23,42 +23,28 @@ public class UserController {
     private final UserService userService;
 
     // 유저 정보 등록
+    @Operation(summary = "회원 가입", description = "새로운 회원을 등록합니다.")
+    @Parameter(name = "UserDto", description = "User 회원 가입을 위한 데이터.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원 가입 성공."),
+            @ApiResponse(responseCode = "400", description = "요청 형식이 올바르지 않습니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(responseCode = "409", description = "이미 가입된 유저입니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class)))
+    })
+
     @PostMapping("signup")
-    public ResponseEntity signupUser(@Valid UserDto userDto, BindingResult result) {
+    public void signupUser(@RequestBody UserDto userDto) {
 
-        // @Valid 검증 과정에서 오류가 발견되면, BAD_REQUEST 처리.
-        if (result.hasErrors()) {
-            Map<String, String> errors = new HashMap<>();
-            result.getAllErrors().forEach((error) -> {
-                String fieldName = ((FieldError) error).getField();
-                String message = error.getDefaultMessage();
-                errors.put(fieldName, message);
-            });
-
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
-        }
-
+        // 유저 데이터 검사 - invalid인 경우 exception 발생
+        userDto.check();
+        
+        // 유저 정보 등록
         userService.registerUser(userDto);
-
-        return RESPONSE_OK;
-    }
-
-    // 닉네임 중복 확인
-    // url 주소에 nickname이 포함되도 괜찮은지 의문이 생긴다.
-    @PostMapping("signup/{nickname}")
-    public ResponseEntity<HttpStatus> checkNickname(@PathVariable String nickname) {
-
-        if (userService.isDuplicatedNickname(nickname)) {
-            return RESPONSE_CONFLICT;
-        }
-
-        return RESPONSE_OK;
     }
 
     /*
     // 이메일 본인 인증
     @PostMapping("verify/{email}")
-    public ResponseEntity<HttpStatus> verifyEmail(@PathVariable String email) {
+    public verifyEmail(@PathVariable String email) {
         if (userService.isDuplicatedEmail(email)) {
 
         } else {
@@ -73,7 +59,7 @@ public class UserController {
     }
 
     @PostMapping("term/{nickname}")
-    public ResponseEntity<HttpStatus> saveTerm(@PathVariable String nickname) {
+    public saveTerm(@PathVariable String nickname) {
 
     }
 
--- mitube-app/src/main/java/com/misim/entity/User.java
+++ mitube-app/src/main/java/com/misim/entity/User.java
@@ -1,7 +1,5 @@
 package com.misim.entity;
 
-import java.time.LocalDateTime;
-
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -9,28 +7,23 @@ import lombok.*;
 @Getter
 @Table(name = "users")
 @NoArgsConstructor
-public class User {
+public class User extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 400)
     private String email;
 
     private String password;
 
     private String nickname;
 
-    private LocalDateTime createDate;
-
-    private LocalDateTime modifyDate;
-
     @Builder
     public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
         this.nickname = nickname;
-        this.createDate = LocalDateTime.now();
-        this.modifyDate = LocalDateTime.now();
     }
 }
