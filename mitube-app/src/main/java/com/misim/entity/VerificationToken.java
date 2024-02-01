e0e1f5d0e5316fb08e539d267e09669e4afce2c2
misim3 <misim@student.42seoul.kr>
2024-02-01 23:26:23 +0900

    refactor: verification 관련 수정

--- mitube-app/src/main/java/com/misim/entity/VerificationToken.java
+++ mitube-app/src/main/java/com/misim/entity/VerificationToken.java
@@ -1,6 +1,7 @@
 package com.misim.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,23 +12,23 @@ import java.time.LocalDateTime;
 @Table(name = "verification_tokens")
 @NoArgsConstructor
 public class VerificationToken {
-    private static final long EXPIRATION_DAY = 1;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private String token;
-
     @OneToOne
     @JoinColumn(name = "USER_ID")
     private User user;
 
-    private LocalDateTime expiryDate;
+    @OneToOne
+    @JoinColumn(name = "SMS_VERIFICATION_ID")
+    private SmsVerification smsVerification;
 
-    public VerificationToken(String token, User user) {
-        this.token = token;
+
+    @Builder
+    public VerificationToken(User user, SmsVerification smsVerification) {
         this.user = user;
-        this.expiryDate = LocalDateTime.now().plusDays(EXPIRATION_DAY);
+        this.smsVerification = smsVerification;
     }
 }
---
+++ mitube-app/src/main/java/com/misim/service/VerificationTokenService.java
@@ -0,0 +1,56 @@
+package com.misim.service;
+
+import com.misim.entity.SmsVerification;
+import com.misim.entity.User;
+import com.misim.entity.VerificationToken;
+import com.misim.exception.MitubeErrorCode;
+import com.misim.exception.MitubeException;
+import com.misim.repository.SmsVerificationRespository;
+import com.misim.repository.VerificationTokenRepository;
+import com.misim.util.Base64Convertor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VerificationTokenService {
+
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final SmsVerificationRespository smsVerificationRespository;
+
+
+    public void setVerificationToken(User user, String token) {
+        SmsVerification smsVerification = smsVerificationRespository
+                .findById(Base64Convertor.decode(token))
+                .orElseThrow(() -> new MitubeException(MitubeErrorCode.NOT_FOUND_SMS_TOKEN));
+
+        VerificationToken verificationToken = VerificationToken.builder()
+                .user(user)
+                .smsVerification(smsVerification)
+                .build();
+
+        verificationTokenRepository.save(verificationToken);
+    }
+
+    public User findUser(String token) {
+
+        SmsVerification smsVerification = smsVerificationRespository
+                .findById(Base64Convertor.decode(token))
+                .orElseThrow(() -> new MitubeException(MitubeErrorCode.NOT_FOUND_SMS_TOKEN));
+
+        VerificationToken verificationToken = verificationTokenRepository.findVerificationTokenBySmsVerificationId(smsVerification.getId());
+
+        return verificationToken.getUser();
+    }
+
+    public String findUserNickname(String token) {
+
+        SmsVerification smsVerification = smsVerificationRespository
+                .findById(Base64Convertor.decode(token))
+                .orElseThrow(() -> new MitubeException(MitubeErrorCode.NOT_FOUND_SMS_TOKEN));
+
+        VerificationToken verificationToken = verificationTokenRepository.findVerificationTokenBySmsVerificationId(smsVerification.getId());
+
+        return verificationToken.getUser().getNickname();
+    }
+}
