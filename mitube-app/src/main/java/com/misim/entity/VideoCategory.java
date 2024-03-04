f69eecbaf17c57ae4b1520c1cbdae3f1eac9fa2c
misim3 <misim@student.42seoul.kr>
2024-03-04 11:23:24 +0900

    feat: 동영상 카테고리 생성

---
+++ mitube-app/src/main/java/com/misim/entity/VideoCategory.java
@@ -0,0 +1,34 @@
+package com.misim.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+@Getter
+@RequiredArgsConstructor
+public enum VideoCategory {
+    MUSIC("음악", 0),
+    GAME("게임", 1),
+    SPORT("스포츠", 2),
+    TECHNOLOGY("과학기술", 3),
+    EDUCATION("교육", 4),
+    STYLE("스타일", 5),
+    NEWS("뉴스", 6),
+    SOCIAL_MOVEMENT("사회운동", 7),
+    ANIMAL("동물", 8),
+    ENTERTAINMENT("엔터테인먼트", 9),
+    TRAVEL("여행", 10),
+    MOVIE("영화", 11),
+    BLOG("블로그", 12),
+    CAR("자동차", 13),
+    COMEDY("코미디", 14),;
+
+    private final String name;
+    private final int code;
+
+    public static Boolean existByCode(int code) {
+        return Arrays.stream(VideoCategory.values())
+                .anyMatch(c -> c.getCode() == code);
+    }
+}
