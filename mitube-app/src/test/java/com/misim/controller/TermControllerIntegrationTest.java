44b85514d359bbfee474652f459f70a4bc189a16
misim3 <misim@student.42seoul.kr>
2024-03-07 20:47:36 +0900

    refactor: 통합 테스트를 위한 코드 수정

--- mitube-app/src/test/java/com/misim/controller/TermControllerIntegrationTest.java
+++ mitube-app/src/test/java/com/misim/controller/TermControllerIntegrationTest.java
@@ -1,13 +1,25 @@
 package com.misim.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.misim.controller.model.Response.TermDetailResponse;
+import com.misim.entity.Term;
+import com.misim.repository.TermRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,13 +29,50 @@ class TermControllerIntegrationTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private TermRepository termRepository;
 
-    @Test
-    void getTerms() {
+    @BeforeEach
+    void setUp() {
+        termRepository.saveAll(Arrays.asList(개인정보동의(), 광고수신동의()));
+    }
+
+    Term 개인정보동의() {
+        return Term.builder()
+                .title("개인정보동의")
+                .content("개인정보동의")
+                .isRequired(true)
+                .version(1)
+                .termGroup(1)
+                .build();
+    }
+
+    Term 광고수신동의() {
+        return Term.builder()
+                .title("광고수신동의")
+                .content("광고수신동의")
+                .isRequired(false)
+                .version(1)
+                .termGroup(2)
+                .build();
     }
 
     @Test
-    void getTermPolicy() {
+    void getTerms() throws Exception {
+        mockMvc.perform(get("/terms/agree")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    // TermControllerIntegrationTest를 실행하면, 상태코드 500이 반환된다.
+    // 아래의 메소드만 실행하면, 원하는 데로 잘 동작한다.
+    // 이 부분에 대한 탐구 필요.
+    @Test
+    void getTermPolicy() throws Exception {
+        mockMvc.perform(get("/terms/policy")
+                        .param("title", "개인정보동의")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 }
--- mitube-app/src/test/java/com/misim/controller/VideoControllerIntegrationTest.java
+++ mitube-app/src/test/java/com/misim/controller/VideoControllerIntegrationTest.java
@@ -1,13 +1,25 @@
 package com.misim.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.misim.controller.model.Request.CreateVideoRequest;
+import com.misim.entity.VideoFile;
+import com.misim.repository.VideoFileRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,12 +31,46 @@ class VideoControllerIntegrationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private VideoFileRepository videoFileRepository;
 
     @Test
-    void uploadVideos() {
+    void uploadVideos() throws Exception {
+
+        MockMultipartFile file = new MockMultipartFile("file", "test-video.mp4", MediaType.MULTIPART_FORM_DATA_VALUE,"test video file".getBytes());
+
+        ResultActions actions = mockMvc.perform(multipart(HttpMethod.POST,"/videos/upload")
+                        .file(file)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf()))
+                .andExpect(status().isOk());
+
+        actions.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.body.id").value("MQ=="));
+
+        Optional<VideoFile> videoFile = videoFileRepository.findById(1L);
+
+        videoFile.ifPresent(i -> System.out.println(i.getPath()));
     }
 
+    // 아래 테스트가 진행되려면, 유저와 업로드된 파일이 필요하다.
+    // Before 어노테이션 활용해서 해결할 수 있을까?
     @Test
-    void createVideos() {
+    void createVideos() throws Exception {
+
+        // mock 객체
+        CreateVideoRequest request = new CreateVideoRequest();
+        request.setTitle("test");
+        request.setDescription("test video");
+        request.setNickname("hongkildong");
+        request.setToken("MQ==");
+        request.setCategoryId(1);
+
+        // 실행 결과 확인
+        mockMvc.perform(post("/videos/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .with(csrf()))
+                .andExpect(status().isOk());
     }
 }
