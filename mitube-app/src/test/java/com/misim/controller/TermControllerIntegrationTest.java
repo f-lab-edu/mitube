97f2c755724ae4eac5fb398b486df25356496889
misim3 <misim@student.42seoul.kr>
2024-03-09 09:23:35 +0900

    refactor: 파라미터로 테스트 분리 및 setUp 메소드 내 약관 저장 한 번만 실행되도록 수정

--- mitube-app/src/test/java/com/misim/controller/TermControllerIntegrationTest.java
+++ mitube-app/src/test/java/com/misim/controller/TermControllerIntegrationTest.java
@@ -33,7 +33,9 @@ class TermControllerIntegrationTest {
 
     @BeforeEach
     void setUp() {
-        termRepository.saveAll(Arrays.asList(개인정보동의(), 광고수신동의()));
+        if (!termRepository.existsById(1L)) {
+            termRepository.saveAll(Arrays.asList(개인정보동의(), 광고수신동의()));
+        }
     }
 
     Term 개인정보동의() {
@@ -57,22 +59,39 @@ class TermControllerIntegrationTest {
     }
 
     @Test
-    void getTerms() throws Exception {
+    void getTermsSuccess() throws Exception {
         mockMvc.perform(get("/terms/agree")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
+                .andExpect(status().isOk());
     }
 
     // TermControllerIntegrationTest를 실행하면, 상태코드 500이 반환된다.
     // 아래의 메소드만 실행하면, 원하는 데로 잘 동작한다.
-    // 이 부분에 대한 탐구 필요.
+    // 이 부분에 대한 탐구 필요.dskfjdsklsdjslkkdjfs1234125
     @Test
-    void getTermPolicy() throws Exception {
+    void getTermPolicySuccess() throws Exception {
         mockMvc.perform(get("/terms/policy")
                         .param("title", "개인정보동의")
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        Optional<Term> term = termRepository.findTermByTitleAndMaxVersion("개인정보동의");
+
+        term.ifPresent(t -> System.out.println(t.getTitle()));
+    }
+
+    @Test
+    void getTermPolicy_hasError_EmptyTitle() throws Exception {
+        mockMvc.perform(get("/terms/policy")
+                        .param("title", "")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void getTermPolicy_hasError_NotFoundTerm() throws Exception {
+        mockMvc.perform(get("/terms/policy")
+                        .param("title", "위치정보동의")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
     }
 }
