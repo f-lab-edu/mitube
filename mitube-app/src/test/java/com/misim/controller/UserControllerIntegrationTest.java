3878cfdec65e34de4ffa0cdd064c647289762b90
misim3 <misim@student.42seoul.kr>
2024-03-09 09:24:29 +0900

    refactor: 초기 테스트 코드 작성

--- mitube-app/src/test/java/com/misim/controller/UserControllerIntegrationTest.java
+++ mitube-app/src/test/java/com/misim/controller/UserControllerIntegrationTest.java
@@ -1,13 +1,27 @@
 package com.misim.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.misim.controller.model.Request.*;
+import com.misim.controller.model.Response.FindNicknameResponse;
+import com.misim.controller.model.Response.VerifySMSResponse;
+import com.misim.util.TimeUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,23 +34,96 @@ class UserControllerIntegrationTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void signupUser() {
+    void signupUser() throws Exception {
+
+        // mock 객체
+        SignUpUserRequest mockRequest = new SignUpUserRequest();
+        mockRequest.setEmail("hongkildong@example.com");
+        mockRequest.setPassword("Qwer1234%");
+        mockRequest.setConfirmPassword("Qwer1234%");
+        mockRequest.setNickname("hongkildong");
+        mockRequest.setPhoneNumber("01012345678");
+        mockRequest.setToken("MQ==");
+
+        // 실행 결과 확인
+        mockMvc.perform(post("/users/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(mockRequest))
+                        .with(csrf()))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void sendSMSVerificationCode() {
+    void sendSMSVerificationCode() throws Exception {
+
+        // mock 객체
+        SendSMSRequest mockRequest = new SendSMSRequest();
+        mockRequest.setPhoneNumber("01012345678");
+
+        // 실행 결과 확인
+        mockMvc.perform(post("/users/sendVerificationBySMS")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(mockRequest))
+                        .with(csrf()))
+                .andExpect(status().isOk());
+    }
+
+    // 테스트를 위해 컨트롤러를 수정하는 것?
+    // 요청 시각을 측정하는 방법?
+    // 요런 플로우를 순서대로 테스트하는 방법?
+    @Test
+    void checkSMSVerificationCode() throws Exception {
+
+        // mock 객체
+        VerifySMSRequest mockRequest = new VerifySMSRequest();
+        mockRequest.setPhoneNumber("01012345678");
+        mockRequest.setCode("123456");
+        LocalDateTime mockCurrent = TimeUtil.getNow();
+        mockRequest.setRequestTime(TimeUtil.formatLocalDateTime(mockCurrent));
+
+        // 실행 결과 확인
+        ResultActions actions = mockMvc.perform(post("/users/verifyAccountSMS")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(mockRequest))
+                        .with(csrf()))
+                .andExpect(status().isOk());
+
+        actions.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.body.token").value("MQ=="));
     }
 
     @Test
-    void checkSMSVerificationCode() {
+    void findNickname() throws Exception {
+
+        // mock 객체
+        FindNicknameRequest mockRequest = new FindNicknameRequest();
+        mockRequest.setToken("MQ==");
+
+        // 실행 결과 확인
+        ResultActions actions = mockMvc.perform(post("/users/nickname/find")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(mockRequest))
+                        .with(csrf()))
+                .andExpect(status().isOk());
+
+        actions.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.body.nickname").value("hongkildong"));
     }
 
     @Test
-    void findNickname() {
-    }
+    void resetPassword() throws Exception {
 
-    @Test
-    void resetPassword() {
+        // mock 객체
+        ResetPasswordRequest mockRequest = new ResetPasswordRequest();
+        mockRequest.setNickname("hongkildong");
+        mockRequest.setCode("MQ==");
+
+        // 실행 결과 확인
+        mockMvc.perform(post("/users/help/resetPassword")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(mockRequest))
+                        .with(csrf()))
+                .andExpect(status().isOk());
     }
 
     @Test
