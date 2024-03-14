d325bd298ea87a70e0d80fd2e2ad6175f76575e3
misim3 <misim@student.42seoul.kr>
2024-03-14 19:23:06 +0900

    refactor: video에서 videoResponse 변환 스태틱 메소드로 생성

--- mitube-app/src/main/java/com/misim/controller/model/Response/VideoResponse.java
+++ mitube-app/src/main/java/com/misim/controller/model/Response/VideoResponse.java
@@ -1,7 +1,11 @@
 package com.misim.controller.model.Response;
 
+import com.misim.entity.Video;
+import com.misim.entity.VideoCategory;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -22,4 +26,18 @@ public class VideoResponse {
     // 시청 중인 유저 정보 - 유저 닉네임과 동영상 시청 시각
 
     private Long views;
+
+    public static List<VideoResponse> convertVideos(List<Video> videos) {
+        return videos.stream()
+            .map(video -> VideoResponse.builder()
+                    .title(video.getTitle())
+                    .description(video.getDescription())
+                    .nickname(video.getUser().getNickname())
+                    .category(VideoCategory.getNameByCode(video.getCategoryId()))
+                    .videoUrl(video.getVideoFile().getPath())
+                    .views(video.getViews())
+                    .thumbnailUrl(video.getThumbnailUrl())
+                    .build())
+            .toList();
+    }
 }
